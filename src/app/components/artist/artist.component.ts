155ac77804929c95fc52752c94ffb324feac6cdd
Gerardo <Gerardo.Sanchez@telefonica.com>
2021-10-10 21:27:38 -0500

    configuracion proyecto

---
+++ src/app/components/artist/artist.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist',
+  templateUrl: './artist.component.html'
+})
+export class ArtistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
